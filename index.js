26f79bb0f2d10d5194f9d0d650a3ae2eff1dacab
HUGO BALVOA ALBARRACIN <70823845+bal0229@users.noreply.github.com>
2021-03-22 22:32:59 -0400

    Update index.js

--- index.js
+++ index.js
@@ -17,8 +17,8 @@ const config = {
     width: 800,
     height: 600,
     //scene:[Game7]
-    scene:[Nivel1]
-    //scene: [Game8,Game4,Nivel1,Game7]
+    //scene:[Nivel1]
+    scene: [Game8,Game4,Nivel1,Game7]
     //scene: [ Boot, Instructions, StackerGame, GameOver ]
 };
 
